9046bde072de83979504f89790aef793c7d7267b
nazmul-islam1412 <mohammad.nazmul.islam1412@gmail.com>
2025-08-02 11:53:10 +0600

    added for task2

---
+++ Solves/for-t2.js
@@ -0,0 +1,12 @@
+console.log("Sub Task 1:");
+for(let i=61;i<=100;i++){
+    if(i%2===1){
+        console.log(i);
+    }
+}
+console.log("Sub Task 2:");
+for(let i=78;i<=98;i++){
+    if(i%2===0){
+        console.log(i);
+    }
+}
