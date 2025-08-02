0cc8a00b11810df3dd6327cb28e2df52a2fe1ee1
nazmul-islam1412 <mohammad.nazmul.islam1412@gmail.com>
2025-08-02 12:20:13 +0600

    for task4

--- Solves/for-t3.js
+++ Solves/for-t3.js
@@ -3,13 +3,16 @@ console.log("Sub Task 1:");
 for(let i=91;i<=129;i++){
     if(i%2===1){
         sum=sum+i;
-        console.log(sum);
+       
     }
 }
+ console.log(sum);
+sum=0;
 console.log("Sub Task 2:");
 for(let i=51;i<=85;i++){
     if(i%2===0){
         sum=sum+i;
-        console.log(sum);
+
     }
 }
+ console.log(sum);
